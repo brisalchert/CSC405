230798ae035fa3dca78b1351e6b956a4e29129c5
Brian Salchert <brisalchert@gmail.com>
2024-12-18 14:28:07 -0500

    Clean up JS comments

--- Module2/gasketMod2.js
+++ Module2/gasketMod2.js
@@ -43,20 +43,21 @@ window.onload = function init() {
         points.push(p);
     }
 
-    // Initialization for WebGL
+    // Initialization for WebGL: create the viewport and
+    // set clear color to white
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    // Load shaders and initialize attribute buffers
+    // Load shaders using WebGL control functions
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    // Load the data to the GPU
+    // Load the data to the GPU using a buffer
     var bufferId = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
 
-    // Associate out shader variables with the data buffer
+    // Associate vertex shader variable with data buffer
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
