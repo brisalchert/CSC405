3e2218563510baa95654a84de0c7d0f76943e45c
Brian Salchert <brisalchert@gmail.com>
2025-01-08 22:11:32 -0500

    Add initial code for rotating colored cube

--- Module4/gasketMod4.js
+++ Module4/gasketMod4.js
@@ -4,8 +4,54 @@
 var canvas;
 /** @type {WebGLRenderingContext} */
 var gl;
-var points;
-var numPoints = 50000;
+
+var numElements = 36;
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [0, 0, 0];
+
+var thetaLoc;
+
+var vertices = [
+    vec4(-0.5, -0.5,  0.5, 1.0),
+    vec4(-0.5,  0.5,  0.5, 1.0),
+    vec4( 0.5,  0.5,  0.5, 1.0),
+    vec4( 0.5, -0.5,  0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5,  0.5, -0.5, 1.0),
+    vec4( 0.5,  0.5, -0.5, 1.0),
+    vec4( 0.5, -0.5, -0.5, 1.0)
+];
+
+var vertexColors = [
+    [0.0, 0.0, 0.0, 1.0],  // black
+    [1.0, 0.0, 0.0, 1.0],  // red
+    [1.0, 1.0, 0.0, 1.0],  // yellow
+    [0.0, 1.0, 0.0, 1.0],  // green
+    [0.0, 0.0, 1.0, 1.0],  // blue
+    [1.0, 0.0, 1.0, 1.0],  // magenta
+    [0.0, 1.0, 1.0, 1.0],  // cyan
+    [1.0, 1.0, 1.0, 1.0]   // white
+];
+
+var indices = [
+    1, 0, 3,
+    3, 2, 1,
+    2, 3, 7,
+    7, 6, 2,
+    3, 0, 4,
+    4, 7, 3,
+    6, 5, 1,
+    1, 2, 6,
+    4, 5, 6,
+    6, 7, 4,
+    5, 4, 0,
+    0, 1, 5
+];
 
 window.onload = function init() {
     canvas = document.getElementById("gl-canvas");
@@ -14,60 +60,64 @@ window.onload = function init() {
         alert("WebGL isn't available");
     }
 
-    // Initialize vertices for gasket
-    var vertices = [
-        vec2(-1, -1),
-        vec2(0, 1),
-        vec2(1, -1)
-    ];
-
-    // Specify starting point p for iterations
-    var u = add(vertices[0], vertices[1]);
-    var v = add(vertices[0], vertices[2]);
-    var p = scale(0.5, add(u, v));
-
-    // Add the initial point to the array of points
-    points = [p];
-
-    // Compute each new point as the midpoint between the last
-    // point and a random vertex
-    for (var i = 0; points.length < numPoints; ++i) {
-        // Select a random vertex index
-        var j = Math.floor(Math.random() * 3);
-
-        // Compute the new point
-        p = add(points[i], vertices[j]);
-        p = scale(0.5, p);
-
-        // Add the new point
-        points.push(p);
-    }
-
-    // Initialization for WebGL: create the viewport and
-    // set clear color to white
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    // Load shaders using WebGL control functions
+    gl.enable(gl.DEPTH_TEST);
+
+    // Load shaders and initialize attribute buffers
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    // Load the data to the GPU using a buffer
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    // Element buffer for triangle fan indices
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 
-    // Associate vertex shader variable with data buffer
+    // Color buffer and attribute
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+
+    var vColor = gl.getAttribLocation(program, "vColor");
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    // Vertex buffer and attribute
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
     var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
+    // Event listeners for buttons
+    document.getElementById("xButton").onclick = function () {
+        axis = xAxis;
+        // theta[xAxis] += 15.0;
+    };
+    document.getElementById("yButton").onclick = function () {
+        axis = yAxis;
+        // theta[yAxis] += 15.0;
+    };
+    document.getElementById("zButton").onclick = function () {
+        axis = zAxis;
+        // theta[zAxis] += 15.0;
+    };
+
     render();
-};
+}
 
 function render() {
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Render the buffer data using points as the primitives
-    gl.drawArrays(gl.POINTS, 0, points.length);
+    theta[axis] += 1.0;
+    gl.uniform3fv(thetaLoc, theta);
+
+    gl.drawElements(gl.TRIANGLES, numElements, gl.UNSIGNED_BYTE, 0);
+
+    requestAnimFrame(render);
 }
