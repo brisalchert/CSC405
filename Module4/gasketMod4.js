61e8d97f1edcf7bfcb1dfd3c116acc5cc642ea39
Brian Salchert <brisalchert@gmail.com>
2025-01-10 12:41:55 -0500

    Update comments for code explanations

--- Module4/gasketMod4.js
+++ Module4/gasketMod4.js
@@ -33,6 +33,8 @@ var scaleToggle = true;
 var colorTheta = 0.0;
 var colorThetaLoc;
 
+// Define the shape of the cube with vertex coordinates
+// in the model frame
 var vertices = [
     vec4(-0.11, -0.11,  0.11, 1.0),
     vec4(-0.11,  0.11,  0.11, 1.0),
@@ -44,6 +46,7 @@ var vertices = [
     vec4( 0.11, -0.11, -0.11, 1.0)
 ];
 
+// Define the initial color values for each vertex
 var vertexColors = [
     [0.0, 0.0, 0.0, 1.0],  // black
     [1.0, 0.0, 0.0, 1.0],  // red
@@ -55,6 +58,8 @@ var vertexColors = [
     [0.0, 1.0, 1.0, 1.0]   // cyan
 ];
 
+// Define the indices for each of the 12 triangles
+// composing the faces of the cube
 var indices = [
     1, 0, 3,
     3, 2, 1,
@@ -77,38 +82,43 @@ window.onload = function init() {
         alert("WebGL isn't available");
     }
 
+    // Set viewport and set background color to black
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     gl.enable(gl.DEPTH_TEST);
 
-    // Load shaders and initialize attribute buffers
+    // Load vertex and fragment shaders
     var program = initShaders(gl, "vertex-shader", "fragment-shader");
     gl.useProgram(program);
 
-    // Element buffer for triangle fan indices
+    // Pass index data to the element array buffer, which handles
+    // element indices rather than vertex attributes
     var iBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
 
-    // Color buffer and attribute
+    // Pass vertex color data to a new array buffer
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
 
+    // Link color data to the attribute in the vertex shader
     var vColor = gl.getAttribLocation(program, "vColor");
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
 
-    // Vertex buffer and attribute
+    // Pass vertex position data to a new array buffer
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
+    // Link position data to the attribute in the vertex shader
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
+    // Get locations of uniforms in the vertex and fragment shaders
     thetaLoc = gl.getUniformLocation(program, "theta");
     translateLoc = gl.getUniformLocation(program, "translate");
     scaleLoc = gl.getUniformLocation(program, "scale");
@@ -125,17 +135,22 @@ window.onload = function init() {
         scaleToggle = !scaleToggle;
     };
 
+    // Start the render loop
     render();
 }
 
 function updateTranslation(translateAxis) {
+    // Set a boundary for the "walls" of the scene
     var boundary = 0.8;
 
+    // Invert translation for given axis and cycle rotation axis
+    // when colliding with a wall
     if (translate[translateAxis] >= boundary || translate[translateAxis] <= -boundary) {
         translatePositive[translateAxis] = !translatePositive[translateAxis];
         axis = (axis + 1) % 3;
     }
 
+    // Apply next translation for this axis
     if (translatePositive[translateAxis]) {
         translate[translateAxis] += translateVelocity[translateAxis];
     } else {
@@ -144,10 +159,12 @@ function updateTranslation(translateAxis) {
 }
 
 function updateScaling() {
+    // Invert scaling if max/min size reached
     if (scale[0] >= 1.75 || scale[0] <= 0.25) {
         scalePositive = !scalePositive;
     }
 
+    // Apply scaling for each dimension
     for (var i = 0; i < scale.length; i++) {
         if (scalePositive) {
             scale[i] += scaleVelocity;
@@ -176,15 +193,18 @@ function render() {
         updateScaling();
     }
 
-    // Handle color
+    // Increment color cycle
     colorTheta = (colorTheta + (2 * Math.PI / 1000)) % (2 * Math.PI);
 
+    // Pass new values to uniforms in the vertex and fragment shaders
     gl.uniform3fv(thetaLoc, theta);
     gl.uniform2fv(translateLoc, translate);
     gl.uniform3fv(scaleLoc, scale);
     gl.uniform1f(colorThetaLoc, colorTheta);
 
+    // Draw the cube
     gl.drawElements(gl.TRIANGLES, numElements, gl.UNSIGNED_BYTE, 0);
 
+    // Start next animation frame
     requestAnimFrame(render);
 }
